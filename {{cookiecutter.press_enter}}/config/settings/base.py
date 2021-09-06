6a83468b43a999d7fb5bfcea8a8b38c23ae1de16
Julian <home@Julians-MBP.lan>
2021-09-06 10:24:26 -0500

    '2021-Sep-06'

--- {{cookiecutter.press_enter}}/config/settings/base.py
+++ {{cookiecutter.press_enter}}/config/settings/base.py
@@ -301,7 +301,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
-        'backend.apps.authentication.backends.JWTAuthentication', # REALWORLD
+        # 'backend.users.backends.JWTAuthentication', # REALWORLD
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
