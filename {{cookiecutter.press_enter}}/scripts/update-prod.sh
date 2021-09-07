12e48506e75b051f9978f840b9e1fab5d0155eda
Julian <home@Julians-MBP.lan>
2021-09-06 19:01:14 -0500

    '2021-Sep-06'

--- {{cookiecutter.press_enter}}/scripts/update-prod.sh
+++ {{cookiecutter.press_enter}}/scripts/update-prod.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd ../
+cd ../
 rsync -avz --exclude-from=backend/exclude.txt backend/ root@{{cookiecutter.server_ip}}:/root/backend/
 sshpass -p "<SERVER PASSWORD HERE>" ssh -o StrictHostKeyChecking=no root@{{cookiecutter.server_ip}} "cd backend; docker-compose -f production.yml down; docker-compose -f production.yml up --build"
